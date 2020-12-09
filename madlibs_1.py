3caf7793b21ae1bb558d12c62d043c18684fa446
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2020-12-09 23:04:02 +0000

    Create madlibs_1.py
    A short and fun madlibs game

---
+++ madlibs_1.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"In life, everything is {adj}! All I do is {verb1} everyday and actually I want to do is {verb2}. Just like {famous_person}!"
+
+print(madlib)
